338260db868d8ad35cecc74623112f747e96c095
Robert Concepcion <robert.concepcion.iii@gmail.com>
2017-12-09 17:44:24 -0500

    refactor out emulator management from main application flow logic

---
+++ buildandwatch/emuManagement.js
@@ -0,0 +1,42 @@
+const process = require('process');
+const spawn = require('child_process').spawn;
+
+/**
+ *  stack of emulator exe's which have
+ *  been launched so far; useful to
+ *  kill all existing processes when 
+ *  launching a new one
+ */
+
+const spawnedEmuPIDs = [];
+
+function closeLaunchedEmuInstances () {
+    while(spawnedEmuPIDs.length > 0) {
+        const pid = spawnedEmuPIDs.pop();
+        try {
+            process.kill(pid);            
+        } catch(err) {
+            console.error(`The emulator process with pid of ${pid} which was ` + 
+            `attempted to be closed no longer exists`);
+        }
+    }
+}
+
+function launchEmu({ emuPath, romPath }) {
+    return new Promise((resolve, reject)=> {
+        closeLaunchedEmuInstances();
+        
+        const emuProcess = spawn(emuPath, [romPath]);
+        spawnedEmuPIDs.push(emuProcess.pid);
+        
+        console.log('launching emulator!');
+    
+        emuProcess.on('exit', function(code) {
+            if(code != 0 && code != 1) {
+                    console.log('emu process exited with code ' + code);
+            }
+        });
+    });
+}
+
+module.exports = { launchEmu };
--- buildandwatch/index.js
+++ buildandwatch/index.js
@@ -1,63 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 const args = require('yargs').argv;
-const process = require('process');
+
+const registerFileChange = require('./registerFileChange');
+const buildROM = require('./buildROM');
+const { launchEmu } = require ('./emuManagement');
 
 const config = require('./../config.json');
-const spawn = require('child_process').spawn;
 
 const inputPath = path.resolve(config.watchFolder, config.inputSourceFile);
 const outputPath = path.resolve(__dirname, './../', config.outputFileName);
 
-const registerFileChange = require('./registerFileChange');
-const buildROM = require('./buildROM');;
-
-
-
-/**
- *  stack of emulator exe's which have
- *  been launched so far; useful to
- *  kill all existing processes when 
- *  launching a new one
- */
-
-const spawnedEmuPIDs = [];
-
-function closeLaunchedEmuInstances () {
-    while(spawnedEmuPIDs.length > 0) {
-        const pid = spawnedEmuPIDs.pop();
-        try {
-            process.kill(pid);            
-        } catch(err) {
-            console.error(`The emulator process with pid of ${pid} which was ` + 
-            `attempted to be closed no longer exists`);
-        }
-    }
-}
-
-function launchEmu() {
-    return new Promise((resolve, reject)=> {
-        closeLaunchedEmuInstances();
-        
-        const emuProcess = spawn(config.emuPath, [outputPath]);
-        spawnedEmuPIDs.push(emuProcess.pid);
-        
-        console.log('launching emulator!');
-    
-        emuProcess.on('exit', function(code) {
-            if(code != 0 && code != 1) {
-                    console.log('emu process exited with code ' + code);
-            }
-        });
+// launch emu with necessary params
+const launchEmuWParams = ()=> {
+    return launchEmu({ 
+        romPath : outputPath, 
+        emuPath : config.emuPath 
     });
-}
+};
 
 // build and launch ROM on start
 // according to configuration settings
 
 if(config.buildOnStart) {
     buildROM({ inputPath, outputPath })
-        .then(config.openEmuOnStart ? launchEmu : null );
+        .then(config.openEmuOnStart ? launchEmuWParams : null );
 
     } else if(config.openEmuOnStart) {
     openEmuOnStart();
@@ -75,7 +42,7 @@ fs.watch(config.watchFolder, (action, filename)=> {
         if(isValidChange) {
             console.log(`c or h file was updated:  ${filename} (${action})`);  
             buildROM({ inputPath, outputPath })
-                .then(config.openEmuOnChange ? launchEmu : null);
+                .then(config.openEmuOnChange ? launchEmuWParams : null);
         }
     }
 });
